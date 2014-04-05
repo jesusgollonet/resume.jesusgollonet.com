44a04c1d69789c354c15454a9e26c8c29586f6be
Jesus Gollonet <jesusgollonet@gmail.com>
2014-04-06 01:24:12 +0200

    Rearrange scripts. Add publish method

--- scripts/util.sh
+++ scripts/util.sh
@@ -1,16 +1,28 @@
 #!bin/bash
+# depends on pandoc
 
 ## up to you to keep this up to date
 function lsmethods(){
 cat << EOF 
-  lsmethods: 
-      list all available methods for the project
-  gencv: 
-      generate and open cv in html
+lsmethods: list all available methods for the project
+generate
+preview
+publish
+      
 EOF
 }
 
+
+function generate(){
+    pandoc resume.md -s -o index.html
+}
+
 # generate && open cv
-function gencv(){
-	pandoc resume.md -s -o resume.html && o resume.html
+function preview(){
+    generate && o index.html
+}
+
+function publish(){
+    generate 
+    scp index.html jgb:public_html/resume.jesusgollonet.com
 }
