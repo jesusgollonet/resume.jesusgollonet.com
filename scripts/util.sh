067a5acb06b6ed32efa36d96bb47bb90df36e4ba
Jesus Gollonet <jesusgollonet@gmail.com>
2014-04-06 00:31:08 +0200

    list methods in local scripts/util.sh

--- scripts/util.sh
+++ scripts/util.sh
@@ -1,5 +1,15 @@
 #!bin/bash
 
+## up to you to keep this up to date
+function lsmethods(){
+cat << EOF 
+  lsmethods: 
+      list all available methods for the project
+  gencv: 
+      generate and open cv in html
+EOF
+}
+
 # generate && open cv
 function gencv(){
 	pandoc resume.md -s -o resume.html && o resume.html
